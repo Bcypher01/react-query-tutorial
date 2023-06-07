3eb1562de09bddc2185029aa324c58d72aa28cc2
Bcypher01 <olumidebabayemi1@gmail.com>
2023-06-07 15:42:05 +0100

    add single page

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,8 +1,9 @@
 import { useHistory, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default function Summary({ character }) {
+export default function Summary() {
   const { id } = useParams();
   const [summary, setSummary] = useState([]);
   let url = `https://api.tvmaze.com/singlesearch/shows?q=${id}`;
@@ -21,17 +22,26 @@ export default function Summary({ character }) {
     };
   }, []);
 
-  return (
-    <div className="border shadow-md">
-      {/* <img src={character.image} alt="" />
-      <div className="text-container">
-        <h3>{character.name}</h3>
-        <p className="status">
-          {character.status} - {character.species}
-        </p>
-        <p className="title">Last seen on</p>
-        <p>{character.location.name}</p>
-      </div> */}
-    </div>
-  );
+  let hs = summary.data?.summary;
+  let theObj = { __html: hs };
+  if (summary.data) {
+    return (
+      <div className="border mx-12 mt-4 flex space-x-12 shadow-md">
+        <img src={summary.data?.image.medium} alt="" />
+        <div>
+          <h1 className="py-2 text-2xl font-semibold">{summary.data?.name}</h1>
+          <div className="pb-4" dangerouslySetInnerHTML={theObj} />
+          <Link className="text-blue-400 pb-4" to="/">
+            Go back to previous page
+          </Link>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <h1 className="font-semibold text-3xl text-center">
+        This movie was not found in our database
+      </h1>
+    );
+  }
 }
