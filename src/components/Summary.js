d5ce78e1a7db41f1d236d8849606fafb8a164737
Bcypher01 <olumidebabayemi1@gmail.com>
2023-06-07 13:21:51 +0100

    add single page

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,9 +1,29 @@
 import { useHistory, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Summary({ character }) {
+  const { id } = useParams();
+  const [summary, setSummary] = useState([]);
+  let url = `https://api.tvmaze.com/singlesearch/shows?q=${id}`;
+
+  useEffect(() => {
+    let mounted = true;
+    axios.get(url).then((data) => {
+      if (mounted) {
+        setSummary(data);
+        console.log(data);
+      }
+    });
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
   return (
-    <div className="border shadow-md" key={character.id}>
-      <img src={character.image} alt="" />
+    <div className="border shadow-md">
+      {/* <img src={character.image} alt="" />
       <div className="text-container">
         <h3>{character.name}</h3>
         <p className="status">
@@ -11,7 +31,7 @@ export default function Summary({ character }) {
         </p>
         <p className="title">Last seen on</p>
         <p>{character.location.name}</p>
-      </div>
+      </div> */}
     </div>
   );
 }
